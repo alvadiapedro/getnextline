aad9feb65457a7be504a095cf43127f2d01aa93a
Pedro Miranda alvadia <pmiranda@c1r5s1.42lisboa.com>
2022-03-11 16:36:57 +0000

    vai estudar e testar, ve mais videos

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,33 @@
 /*   By: pmiranda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 14:55:15 by pmiranda          #+#    #+#             */
-/*   Updated: 2022/02/22 14:55:19 by pmiranda         ###   ########.fr       */
+/*   Updated: 2022/03/11 16:36:25 by pmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+char *get_next_line(int fd)
+{
+	int			bytes;
+	char		storage[BUFFER_SIZE + 1];
+	static char	*str[OPEN_MAX];
+	char		*temp;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	bytes = read(fd, storage, BUFFER_SIZE);
+	while (bytes < 0)
+	{
+		storage[bytes] = '\0';
+		if (str[fd] == NULL)
+			s[fd] = ft_strdup("");
+		temp = ft_strjoin(str[fd], storage);
+		free(str[fd]);
+		str[fd] = temp;
+		if (new_line(s[fd]) != -1)
+			break ;
+		bytes = read(fd, storage, BUFFER_SIZE);
+	}
+	return (line_ret(&s[fd], bytes));
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,23 @@
 /*   By: pmiranda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 14:55:51 by pmiranda          #+#    #+#             */
-/*   Updated: 2022/02/22 14:55:55 by pmiranda         ###   ########.fr       */
+/*   Updated: 2022/03/11 16:36:27 by pmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <limits.h>
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+
+char	*get_next_line(int fd);
+
+#endif
